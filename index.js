bef3f6782888c23ca9541b86181e6caa5ca7b783
yalab <rudeboyjet@gmail.com>
2016-01-24 15:46:04 +0900

    Give up to add s3 object architecture. It use sha1 filename.

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
 var aws = require('aws-sdk');
 var s3 = new aws.S3({apiVersion: '2006-03-01'});
 var moment = require('moment');
+var sha1 = require('sha1');
 var s3Bucket = "";
 
 exports.handler = function(event, context) {
   var body = "";
-  var key = moment().format("YYYYMMDDHH") + ".txt";
-  s3.getObject({Bucket: s3Bucket, Key: key})
-  .on("httpData", function(chunk, response){
-    if(response.httpResponse.statusCode === 200){
-      body += chunk;
-    }
-  })
-  .on("httpDone", function(){
-    if(body.length > 0){
-      body += "\n";
-    }
-    body += JSON.stringify(event);
-    s3.putObject({
-      Bucket: s3Bucket,
-      Key: key,
-      Body: new Buffer(body, 'binary'),
-      ContentType: 'application/octet-stream'
-    }, function(err, result){
-         if(err){
-           context.done(err, "s3 put error");
-         }else{
-           context.done(null, 'success');
-         }
-       });
-  })
-  .send();
+  var dir = moment().format("YYYYMMDDHH");
+  var fname = "";
+  if(body.length > 0){
+    body += "\n";
+  }
+  body += JSON.stringify(event);
+  s3.putObject({
+    Bucket: s3Bucket,
+    Key: dir + '/' + sha1(body) + ".log",
+    Body: new Buffer(body, 'binary'),
+    ContentType: 'application/octet-stream'
+  }, function(err, result){
+       if(err){
+         context.done(err, "s3 put error");
+       }else{
+         context.done(null, 'success');
+       }
+     });
 };
 
-//exports.handler({hoge: 'foo'}, {done: function(){}});
+// exports.handler({hoge: 'bar'}, {done: function(){}});
